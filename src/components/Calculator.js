1137a991e1b77a067f811886482c8e0784d5babe
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-09-12 05:36:40 +0500

    Added child component to js file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,47 @@
-const Calculator = () => (
-  <div className="outer-container">
-    <div className="header-calc">
-      <p>0</p>
-    </div>
-    <Button />
-  </div>
+import React, { Component } from 'react';
+
+const Button = (props) => (
+  <button type="button" className={`btn ${props.className}`} name={props.name}>
+    {props.label}
+  </button>
 );
 
-const Button = () => (
-  <div className="button-container">
-    <button type="button" className="btn" name="AC">AC</button>
-    <button type="button" className="btn" name="+/-">+/-</button>
-    <button type="button" className="btn" name="%">%</button>
-    <button type="button" className="orange" name="÷">÷</button>
-    <br />
-    <button type="button" className="btn" name="7">7</button>
-    <button type="button" className="btn" name="8">8</button>
-    <button type="button" className="btn" name="9">9</button>
-    <button type="button" className="orange" name="*">*</button>
-    <br />
-    <button type="button" className="btn" name="4">4</button>
-    <button type="button" className="btn" name="5">5</button>
-    <button type="button" className="btn" name="6">6</button>
-    <button type="button" className="orange" name="-">-</button>
-    <br />
-    <button type="button" className="btn" name="1">1</button>
-    <button type="button" className="btn" name="2">2</button>
-    <button type="button" className="btn" name="3">3</button>
-    <button type="button" className="orange" name="+">+</button>
-    <br />
-    <button type="button" className="zero-btn" name="0">0</button>
-    <button type="button" className="btn" name=".">.</button>
-    <button type="button" className="orange" name="=">=</button>
-  </div>
-);
+class Calculator extends Component {
+  render() {
+    return (
+      <div className="outer-container">
+        <div className="header-calc">
+          <p>0</p>
+        </div>
+        <div className="button-container">
+          {/* Use the Button component for each button */}
+          <Button className="" name="AC" label="AC" />
+          <Button className="" name="+/-" label="+/-" />
+          <Button className="" name="%" label="%" />
+          <Button className="orange" name="÷" label="÷" />
+          <br />
+          <Button className="" name="7" label="7" />
+          <Button className="" name="8" label="8" />
+          <Button className="" name="9" label="9" />
+          <Button className="orange" name="*" label="*" />
+          <br />
+          <Button className="" name="4" label="4" />
+          <Button className="" name="5" label="5" />
+          <Button className="" name="6" label="6" />
+          <Button className="orange" name="-" label="-" />
+          <br />
+          <Button className="" name="1" label="1" />
+          <Button className="" name="2" label="2" />
+          <Button className="" name="3" label="3" />
+          <Button className="orange" name="+" label="+" />
+          <br />
+          <Button className="zero-btn" name="0" label="0" />
+          <Button className="" name="." label="." />
+          <Button className="orange" name="=" label="=" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Calculator;
