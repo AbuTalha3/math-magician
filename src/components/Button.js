72077f6c368211363e13a0a5bee04e5ae6fa2aea
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-09-14 00:19:01 +0500

    Add event functionalities to the calculator

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,29 +1,17 @@
-const Button = () => (
-  <div className="button-container">
-    <button type="button" className="btn" name="AC">AC</button>
-    <button type="button" className="btn" name="+/-">+/-</button>
-    <button type="button" className="btn" name="%">%</button>
-    <button type="button" className="orange" name="÷">÷</button>
-    <br />
-    <button type="button" className="btn" name="7">7</button>
-    <button type="button" className="btn" name="8">8</button>
-    <button type="button" className="btn" name="9">9</button>
-    <button type="button" className="orange" name="*">*</button>
-    <br />
-    <button type="button" className="btn" name="4">4</button>
-    <button type="button" className="btn" name="5">5</button>
-    <button type="button" className="btn" name="6">6</button>
-    <button type="button" className="orange" name="-">-</button>
-    <br />
-    <button type="button" className="btn" name="1">1</button>
-    <button type="button" className="btn" name="2">2</button>
-    <button type="button" className="btn" name="3">3</button>
-    <button type="button" className="orange" name="+">+</button>
-    <br />
-    <button type="button" className="zero-btn" name="0">0</button>
-    <button type="button" className="btn" name=".">.</button>
-    <button type="button" className="orange" name="=">=</button>
-  </div>
-);
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+
+// eslint-disable-next-line react/prop-types
+function Button({ onClick, label }) {
+  return (
+    <button type="button" onClick={onClick}>
+      {label}
+    </button>
+  );
+}
+
+Button.protoTypes = {
+  label: PropTypes.string.isRequired,
+};
 
 export default Button;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,15 +1,77 @@
+// import Button from './Button';
+// import calculate from '../logic/calculate';
+
+// const Calculator = () => (
+//   <div className="outer-container">
+//     <Screen />
+//     <Button />
+//   </div>
+// );
+
+// const Screen = () => (
+//   <div className="header-calc">
+//     <p>0</p>
+//   </div>
+// );
+// export default Calculator;
+
+import React, { useState } from 'react';
 import Button from './Button';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="outer-container">
-    <Screen />
-    <Button />
-  </div>
-);
+function Calculator() {
+  const [calc, setCalc] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  const handleClick = (buttonName) => {
+    let newData = calculate(calc, buttonName);
+    if (buttonName === '=') {
+      newData = { ...newData, operation: null };
+    }
+    setCalc(newData);
+  };
+  return (
+    <div className="outer-container">
+      <div className="header-calc">
+        {calc.total}
+        {calc.operation}
+        {calc.next}
+      </div>
+      <div className="button-container">
+        <div className="first-line">
+          <Button onClick={() => handleClick('AC')} className="btn" label="AC" />
+          <Button onClick={() => handleClick('+/-')} className="btn" label="+/-" />
+          <Button onClick={() => handleClick('%')} className="btn" label="%" />
+          <Button onClick={() => handleClick('÷')} className="orange" label="÷" />
+        </div>
+        <div className="first-line">
+          <Button onClick={() => handleClick('7')} className="btn" label="7" />
+          <Button onClick={() => handleClick('8')} className="btn" label="8" />
+          <Button onClick={() => handleClick('9')} className="btn" label="9" />
+          <Button onClick={() => handleClick('*')} className="orange" label="*" />
+        </div>
+        <div className="first-line">
+          <Button onClick={() => handleClick('4')} className="btn" label="4" />
+          <Button onClick={() => handleClick('5')} className="btn" label="5" />
+          <Button onClick={() => handleClick('6')} className="btn" label="6" />
+          <Button onClick={() => handleClick('-')} className="orange" label="-" />
+        </div>
+        <div className="first-line">
+          <Button onClick={() => handleClick('1')} className="btn" label="1" />
+          <Button onClick={() => handleClick('2')} className="btn" label="2" />
+          <Button onClick={() => handleClick('3')} className="btn" label="3" />
+          <Button onClick={() => handleClick('+')} className="orange" label="+" />
+        </div>
+        <div className="first-line button-zero">
+          <Button onClick={() => handleClick('0')} className="zero-btn" label="0" />
+          <Button onClick={() => handleClick('.')} className="btn" label="." />
+          <Button onClick={() => handleClick('=')} className="orange" label="=" />
+        </div>
+      </div>
+    </div>
+  );
+}
 
-const Screen = () => (
-  <div className="header-calc">
-    <p>0</p>
-  </div>
-);
 export default Calculator;
