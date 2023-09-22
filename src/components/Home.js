1b50307f9629da3d2a4d004d29f20c46e5bf61aa
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-09-22 22:54:25 +0500

    add requred style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable max-len */
 import React from 'react';
 
 function Home() {
   return (
+    // eslint-disable-next-line react/jsx-no-comment-textnodes
     <div>
-      <h1>Home</h1>
-      <p>The amount of eccentricity in a society has generally been proportional of genius.</p>
+      <h1>Welcome to our page!</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in nisl quis leo consectetur accumsan nec vel nunc. Vestibulum dictum metus sit amet consequat lacinia. Nunc odio purus, porttitor non auctor et, pulvinar id lorem. In nisl ligula, sagittis vitae lorem et, auctor laoreet lectus. Sed eu velit commodo, porttitor risus id, vestibulum dui. Integer sollicitudin diam eu rutrum pellentesque. Vivamus consequat nisl libero, vel dapibus sem feugiat sit amet.The amount of eccentricity in a society has generally been proportional of genius.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in nisl quis leo consectetur accumsan nec vel nunc. Vestibulum dictum metus sit amet consequat lacinia. Nunc odio purus, porttitor non auctor et, pulvinar id lorem. In nisl ligula, sagittis vitae lorem et, auctor laoreet lectus. Sed eu velit commodo, porttitor risus id, vestibulum dui. Integer sollicitudin diam eu rutrum pellentesque. Vivamus consequat nisl libero, vel dapibus sem feugiat sit amet.The amount of eccentricity in a society has generally been proportional of genius.</p>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 function NavBar() {
   return (
     <div>
-      <h1>Calculator App</h1>
+      <h1>Math Magicians</h1>
       <div>
-        <ul>
+        <ul className="list">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/calculator">Calculator</Link></li>
           <li><Link to="/quote">Quote</Link></li>
